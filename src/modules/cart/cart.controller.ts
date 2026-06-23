300a5cb8a56eb5562d0c6b5fccb1b3b647552343
Mohamed Nabil <mohamed.nabil67999@gmail.com>
2026-06-23 23:32:24 +0300

    feat(cart): implement cart module

---
+++ src/modules/cart/cart.controller.ts
@@ -0,0 +1,75 @@
+import { Request, Response } from "express";
+import { asyncHandler } from "../../utils/async-handler";
+import { cartService } from "./cart.service";
+import { success } from "zod";
+
+const getCart = asyncHandler(async (req: Request, res: Response) => {
+  const userId = req.user!.id;
+  const cart = await cartService.getCart(userId);
+
+  res.status(200).json({
+    success: true,
+    data: cart,
+  });
+});
+
+const addProduct = asyncHandler(async (req: Request, res: Response) => {
+  const userId = req.user!.id;
+  const productId = Number(req.params.productId);
+  const cartItem = await cartService.addProduct(userId, productId);
+
+  res.status(201).json({
+    success: true,
+    message: "Product added to cart successfully",
+    data: cartItem,
+  });
+});
+
+const updateProductQuantity = asyncHandler(async (req: Request, res: Response) => {
+  const userId = req.user!.id;
+  const productId = Number(req.params.productId);
+  const { quantity } = req.body;
+
+  const cartItem = await cartService.updateProductQuantity(
+    userId,
+    productId,
+    quantity,
+  );
+
+  res.status(200).json({
+    success: true,
+    message: "Cart updated successfully",
+    data: cartItem,
+  });
+});
+
+const removeProduct = asyncHandler(async (req: Request, res: Response) => {
+  const userId = req.user!.id;
+  const productId = Number(req.params.productId);
+  const removedItem = await cartService.removeProduct(userId, productId);
+
+  res.status(200).json({
+    success: true,
+    message: "Product removed from cart successfully",
+    data: removedItem,
+  });
+});
+
+const clearCart = asyncHandler(async (req: Request, res: Response) => {
+  const userId = req.user!.id;
+
+  await cartService.clearCart(userId);
+
+  res.status(200).json({
+    success: true,
+    message: "Cart cleared successfully",
+  });
+});
+
+export const cartController = {
+  getCart,
+  addProduct,
+  updateProductQuantity,
+  removeProduct,
+  clearCart,
+};
